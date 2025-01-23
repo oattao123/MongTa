15ba59f887ef400cf42a89d188f587f9b88a826e
oattao123 <72804848+oattao123@users.noreply.github.com>
2025-01-23 22:08:25 +0700

    Supports using Google Maps in ios

--- frontend/ios/Runner/AppDelegate.swift
+++ frontend/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,12 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // เพิ่ม Google Maps API Key ที่นี่
+    GMSServices.provideAPIKey("AIzaSyBqCSDj6uncmfNSNQn2an_10WD2IRTpPZU")
+    
+    // ลงทะเบียน Plugins
     GeneratedPluginRegistrant.register(with: self)
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
